67faec70da3b49ff6dc08779a5ebd7d6da348082
rastabrandy02 <73582888+rastabrandy02@users.noreply.github.com>
2021-12-13 12:46:53 +0100

    Euler angles to rotation and back

---
+++ Lab03/Euler_Angles_To_Matrix_And_Back/Euler_Angles_To_Matrix_And_Back.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+float alpha;
+float beta;
+float gamma;
+
+float rotationMatrix[3][3] = { 0 };
+float alphaResult;
+float betaResult;
+float gammaResult;
+
+void GetRotationFromEulerAngles(float alpha, float beta, float gamma, float result[][3]);
+void GetEulerAnglesfromRotation(float matrix[][3], float& alpha, float& beta, float& gamma);
+
+
+int main()
+{
+	cout << "This program implements a function to pass from a set of Euler angles to it's rotation matrix" << endl;
+	cout << "The second part of this program implements a function that does the inverse process, now using the rotation matrix obtained before" << endl;
+
+	cout << "Introduce the ALPHA (yaw) angle:" << endl;
+	cin >> alpha;
+	cout << "Introduce the BETA (pitch) angle:" << endl;
+	cin >> beta;
+	cout << "Introduce the GAMMA (roll) angle:" << endl;
+	cin >> gamma;
+	cout << endl << "Computing the rotation matrix..." << endl<< endl;
+
+	GetRotationFromEulerAngles(alpha, beta, gamma, rotationMatrix);
+	cout << "This is the rotation matrix obtained: " << endl;
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			cout <<" " << rotationMatrix[i][j];
+		}
+		cout << endl;
+	}
+
+	cout << "Obtaining the euler angles from the rotation matrix..." << endl;
+	GetEulerAnglesfromRotation(rotationMatrix, alphaResult, betaResult, gammaResult);
+	cout << "ALPHA: " << alphaResult << endl;
+	cout << "BETA: " << betaResult << endl;
+	cout << "GAMMA: " << gammaResult << endl;
+
+	return 0;
+}
+void GetRotationFromEulerAngles(float alpha, float beta, float gamma, float result[][3])
+{
+	result[0][0] = cos(gamma) * cos(beta);
+	result[0][1] = cos(gamma) * sin(beta) * sin(alpha) - sin(gamma) * cos(alpha);
+	result[0][2] = cos(gamma) * sin(beta) * cos(alpha) + sin(gamma) * sin(alpha);
+
+	result[1][0] = sin(gamma) * cos(beta);
+	result[1][1] = sin(gamma) * sin(beta) * sin(alpha) + cos(gamma) * cos(alpha);
+	result[1][2] = sin(gamma) * sin(beta) * cos(alpha) - cos(gamma) * sin(alpha);
+
+	result[2][0] = -sin(beta);
+	result[2][1] = cos(beta) * sin(alpha);
+	result[2][2] = cos(beta) * cos(alpha);
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			if (result[i][j] > -0.01f && result[i][j] < 0.01f) result[i][j] = 0.0f;
+		}
+	}
+}
+void GetEulerAnglesfromRotation(float matrix[][3], float &alpha, float &beta, float &gamma)
+{
+	beta = -asin(matrix[2][0]);
+	alpha = (acos(matrix[0][0] / cos(beta)) + asin(matrix[1][0] / cos(beta))) / 2;
+	gamma = (asin(matrix[2][1] / cos(beta)) + acos(matrix[2][2] / cos(beta))) / 2;
+
+}
